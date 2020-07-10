1dd1830ecfa9c0096b0447680a7d86ff4c07c79a
DaegyunMoon <moondg1019@gmail.com>
2020-07-10 17:11:09 +0900

    Add Image Swiper Using swiper.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+// 헤더 영역 - 메뉴 보이기/숨기기
 const toggleBtn = document.querySelector('.navbar-toggleBtn');
 const menu = document.querySelector('.navbar-menu');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('hide');
 });
+
+// 대문 영역 - 이미지 삽입
+function initImage() {
+    var imgs = new Array();
+    for (var i = 0; i < 4; i++) {
+        imgs[i] = document.getElementById('img' + (i + 1));
+        imgs[i].src = "image/main0" + (i + 1) + ".jpg";
+    }
+}
+
+initImage();
